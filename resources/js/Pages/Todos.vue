8ffcb7153af22c47fb5a9c31001a3349b23ec7ea
Daniel Pecher <dan.pecher@gmail.com>
2024-01-07 19:17:51 +0100

    Add icon

--- resources/js/Pages/Todos.vue
+++ resources/js/Pages/Todos.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
-import { router } from "@inertiajs/vue3";
+import { router } from "@inertiajs/vue3"
+import { TrashIcon } from "@heroicons/vue/24/outline"
 
 defineProps({ todos: Array })
 
@@ -45,8 +46,8 @@ function updateTodo(todo) {
             <input v-model="todoText" type="text" placeholder="Create a todo ..." class="border border-gray-200 rounded-md h-10 my-5 mx-1 px-3 py-1 w-full shadow" />
         </form>
 
-        <div class="flex space-x-4 border-b border-gray-200 px-4 py-3" v-for="todo in todos" key="todo.id">
-            <input type="checkbox" :checked="todo.is_completed" v-on:change="() => toggleTodo(todo.id, todo.is_completed)" class="w-6 accent-green-700 rounded-full" />
+        <div class="flex space-x-4 border-b border-gray-200 px-4 py-1 items-center" v-for="todo in todos" key="todo.id">
+            <input type="checkbox" :checked="todo.is_completed" v-on:change="() => toggleTodo(todo.id, todo.is_completed)" class="h-full w-5 accent-green-700 rounded-full" />
 
             <div class="flex-grow" :class="{ 'line-through': todo.is_completed, 'opacity-50': todo.is_completed }">
                 <span v-if="todo.is_completed">
@@ -58,7 +59,9 @@ function updateTodo(todo) {
                 </form>
             </div>
 
-            <button @click="deleteTodo(todo.id)">Delete</button>
+            <button @click="deleteTodo(todo.id)" class="p-2 text-gray-400 hover:text-red-700">
+                <TrashIcon class="h-6 w-6" />
+            </button>
         </div>
 
         <div v-if="todos.length === 0" class="text-gray-700 text-center flex flex-grow items-center justify-center">
