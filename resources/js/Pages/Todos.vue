ed45ae7ff4bd258a737d8d54f9d616b641c92323
Daniel Pecher <dan.pecher@gmail.com>
2024-01-07 18:52:40 +0100

    Add tailwind

--- resources/js/Pages/Todos.vue
+++ resources/js/Pages/Todos.vue
@@ -32,21 +32,24 @@ function submit() {
 </script>
 
 <template>
-    <div>
+    <div class="container mx-auto flex flex-col h-screen">
+        <h1 class="text-6xl font-semibold">Tasks</h1>
+
         <form @submit.prevent="submit">
-            <input v-model="todoText" type="text" />
-            <button type="submit">Create</button>
+            <input v-model="todoText" type="text" placeholder="Create a todo ..." class="border-b border-gray-200 rounded h-10 mt-4 mb-1 mx-1 px-3 py-1 w-full" />
         </form>
 
-        <ul>
-            <li v-for="todo in todos" key="todo.id">
-                <span :style="{textDecoration: todo.is_completed ? 'line-through' : ''}">{{ todo.text }}</span>
-                &nbsp;
-                <input type="checkbox" :checked="todo.is_completed" v-on:change="() => toggleTodo(todo.id, todo.is_completed)" />
-                &nbsp;
-                <button @click="deleteTodo(todo.id)">Delete</button>
-            </li>
-        </ul>
+        <div class="flex space-x-4 border-b border-gray-200 px-4 py-2" :class="{ 'opacity-50': todo.is_completed }" v-for="todo in todos" key="todo.id">
+            <input type="checkbox" :checked="todo.is_completed" v-on:change="() => toggleTodo(todo.id, todo.is_completed)" />
+            <div class="flex-grow" :class="{ 'line-through': todo.is_completed }">
+                {{ todo.text }}
+            </div>
+            <button @click="deleteTodo(todo.id)">Delete</button>
+        </div>
+
+        <div v-if="todos.length === 0" class="text-gray-700 text-center flex flex-grow items-center justify-center">
+            <div>Nothing to see here</div>
+        </div>
     </div>
 </template>
 
