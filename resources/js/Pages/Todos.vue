049df8322330d59b5d0338e63f85dfd6f802e78d
Daniel Pecher <dan.pecher@gmail.com>
2024-01-07 19:46:04 +0100

    Show delete on hover

--- resources/js/Pages/Todos.vue
+++ resources/js/Pages/Todos.vue
@@ -46,7 +46,7 @@ function updateTodo(todo) {
             <input v-model="todoText" type="text" placeholder="What to do..." class="border border-gray-200 rounded-md h-10 my-5 mx-1 px-3 py-1 w-full shadow text-lg font-medium placeholder:font-normal placeholder:text-base" />
         </form>
 
-        <div class="flex space-x-4 border-b border-gray-200 px-4 py-1 items-center hover:bg-gray-100" v-for="todo in todos" key="todo.id">
+        <div class="group flex space-x-4 border-b border-gray-200 px-4 py-1 items-center hover:bg-gray-100" v-for="todo in todos" key="todo.id">
             <input type="checkbox" :checked="todo.is_completed" v-on:change="() => toggleTodo(todo.id, todo.is_completed)" class="h-full w-5 accent-green-700 rounded-full" />
 
             <div class="flex-grow" :class="{ 'line-through': todo.is_completed, 'opacity-50': todo.is_completed }">
@@ -59,7 +59,7 @@ function updateTodo(todo) {
                 </form>
             </div>
 
-            <button @click="deleteTodo(todo.id)" class="p-2 text-gray-400 hover:text-red-700">
+            <button @click="deleteTodo(todo.id)" class="p-2 text-gray-400 hover:text-red-700 opacity-0 group-hover:opacity-100">
                 <TrashIcon class="h-6 w-6" />
             </button>
         </div>
