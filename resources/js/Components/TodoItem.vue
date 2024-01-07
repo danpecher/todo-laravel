4eac54e7c68ef676c7f65b9e3fe6e1ecf466c42f
Daniel Pecher <dan.pecher@gmail.com>
2024-01-07 20:28:41 +0100

    Extract todo item component

---
+++ resources/js/Components/TodoItem.vue
@@ -0,0 +1,42 @@
+<script setup>
+import {TrashIcon} from "@heroicons/vue/24/outline/index.js";
+import {ref} from "vue";
+
+const props = defineProps(['text', 'is_completed'])
+const emit = defineEmits(['onToggle', 'onUpdate', 'onDelete'])
+
+const text = ref(props.text)
+
+function handleUpdate() {
+    if (text.value.length > 0) {
+        emit('onUpdate', text.value)
+    } else {
+        text.value = props.text
+    }
+}
+
+</script>
+
+<template>
+    <div class="group flex space-x-4 border-b border-gray-200 px-4 py-1 items-center hover:bg-gray-100" >
+        <input type="checkbox" :checked="is_completed" v-on:change="$emit('onToggle')" class="h-full w-5 accent-green-700 rounded-full" />
+
+        <div class="flex-grow" :class="{ 'line-through': is_completed, 'opacity-50': is_completed }">
+                <span v-if="is_completed">
+                    {{ text }}
+                </span>
+
+            <form v-else @submit.prevent="handleUpdate">
+                <input @focusout="handleUpdate" class="w-full bg-transparent" type="text" v-model="text" />
+            </form>
+        </div>
+
+        <button @click="$emit('onDelete')" class="p-2 text-gray-400 hover:text-red-700 opacity-0 group-hover:opacity-100">
+            <TrashIcon class="h-6 w-6" />
+        </button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Todos.vue
+++ resources/js/Pages/Todos.vue
@@ -1,13 +1,9 @@
 <script setup>
-import { ref } from 'vue'
 import { router } from "@inertiajs/vue3"
-import { TrashIcon } from "@heroicons/vue/24/outline"
 import TodoInput from "../Components/TodoInput.vue";
+import TodoItem from "../Components/TodoItem.vue";
 
 defineProps({ todos: Array })
-
-// State
-const todoText = ref('')
 
 // Methods
 function toggleTodo(id, currentState) {
@@ -22,13 +18,13 @@ function deleteTodo(id) {
 
 function createTodo(text) {
     router.post('/todos', {
-        text: text
+        text
     })
 }
 
-function updateTodo(todo) {
-    router.patch(`/todos/${todo.id}`, {
-        text: todo.text
+function updateTodo(id, text) {
+    router.patch(`/todos/${id}`, {
+        text
     })
 }
 </script>
@@ -39,23 +35,14 @@ function updateTodo(todo) {
 
         <TodoInput @on-create="text => createTodo(text)" />
 
-        <div class="group flex space-x-4 border-b border-gray-200 px-4 py-1 items-center hover:bg-gray-100" v-for="todo in todos" key="todo.id">
-            <input type="checkbox" :checked="todo.is_completed" v-on:change="() => toggleTodo(todo.id, todo.is_completed)" class="h-full w-5 accent-green-700 rounded-full" />
-
-            <div class="flex-grow" :class="{ 'line-through': todo.is_completed, 'opacity-50': todo.is_completed }">
-                <span v-if="todo.is_completed">
-                    {{ todo.text }}
-                </span>
-
-                <form v-else @submit.prevent="() => updateTodo(todo)">
-                    <input @focusout="() => updateTodo(todo)" class="w-full bg-transparent" type="text" v-model="todo.text" />
-                </form>
-            </div>
-
-            <button @click="deleteTodo(todo.id)" class="p-2 text-gray-400 hover:text-red-700 opacity-0 group-hover:opacity-100">
-                <TrashIcon class="h-6 w-6" />
-            </button>
-        </div>
+        <TodoItem
+            v-for="todo in todos" :key="todo.id"
+            :text="todo.text"
+            :is_completed="todo.is_completed"
+            @on-toggle="() => toggleTodo(todo.id, todo.is_completed)"
+            @on-update="text => updateTodo(todo.id, text)"
+            @on-delete="() => deleteTodo(todo.id)"
+        />
 
         <div v-if="todos.length === 0" class="text-gray-700 text-center flex flex-grow items-center justify-center">
             <div>Nothing to see here</div>
