16023fe03aed6052fb877d73eeda8fdfd10af133
Daniel Pecher <dan.pecher@gmail.com>
2024-01-07 19:11:14 +0100

    Todo updating

--- resources/js/Pages/Todos.vue
+++ resources/js/Pages/Todos.vue
@@ -29,21 +29,35 @@ function submit() {
 
     todoText.value = ''
 }
+
+function updateTodo(todo) {
+    router.patch(`/todos/${todo.id}`, {
+        text: todo.text
+    })
+}
 </script>
 
 <template>
-    <div class="container mx-auto flex flex-col h-screen">
-        <h1 class="text-6xl font-semibold">Tasks</h1>
+    <div class="container mx-auto flex flex-col h-screen my-8">
+        <h1 class="text-4xl font-semibold mx-2">Tasks</h1>
 
         <form @submit.prevent="submit">
-            <input v-model="todoText" type="text" placeholder="Create a todo ..." class="border-b border-gray-200 rounded h-10 mt-4 mb-1 mx-1 px-3 py-1 w-full" />
+            <input v-model="todoText" type="text" placeholder="Create a todo ..." class="border border-gray-200 rounded-md h-10 my-5 mx-1 px-3 py-1 w-full shadow" />
         </form>
 
-        <div class="flex space-x-4 border-b border-gray-200 px-4 py-2" :class="{ 'opacity-50': todo.is_completed }" v-for="todo in todos" key="todo.id">
-            <input type="checkbox" :checked="todo.is_completed" v-on:change="() => toggleTodo(todo.id, todo.is_completed)" />
-            <div class="flex-grow" :class="{ 'line-through': todo.is_completed }">
-                {{ todo.text }}
+        <div class="flex space-x-4 border-b border-gray-200 px-4 py-3" v-for="todo in todos" key="todo.id">
+            <input type="checkbox" :checked="todo.is_completed" v-on:change="() => toggleTodo(todo.id, todo.is_completed)" class="w-6 accent-green-700 rounded-full" />
+
+            <div class="flex-grow" :class="{ 'line-through': todo.is_completed, 'opacity-50': todo.is_completed }">
+                <span v-if="todo.is_completed">
+                    {{ todo.text }}
+                </span>
+
+                <form v-else @submit.prevent="() => updateTodo(todo)">
+                    <input @focusout="() => updateTodo(todo)" class="w-full" type="text" v-model="todo.text" />
+                </form>
             </div>
+
             <button @click="deleteTodo(todo.id)">Delete</button>
         </div>
 
